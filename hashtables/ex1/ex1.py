0043aa2782f83383015d500cd45bf15668521a1f
MattGThomas <mgthomas0616@gmail.com>
2020-04-19 18:36:22 -0500

    finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,17 +1,38 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
+                        # hash_table_remove,
                         hash_table_retrieve,
-                        hash_table_resize)
+                        # hash_table_resize)
+                        )
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
     """
-    YOUR CODE HERE
+    function finds two items whose sum equals the weight limit
+    returns answer as a tuple ie (zero, one)
+
     """
+
+    for i in range(0, len(weights)):
+        # checks the value of limit - weights
+        value = hash_table_retrieve(ht, limit - weights[i])
+        if value != None:
+            return (i, value)
+        else:
+            hash_table_insert(ht, weights[i], i)
+
+    # for i in range(length):
+    #     hash_table_insert(ht, weights[i], i)
+    # for i in range(length):
+    #     checked_weight = hash_table_retrieve(ht, (limit-weights[i]))
+    #     if checked_weight:
+    #         if checked_weight > 1:
+    #             return [checked_weight, i]
+    #         else:
+    #             return [i, checked_weight]
 
     return None
 
